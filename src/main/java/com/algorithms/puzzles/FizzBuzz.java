7a24c55ccd8a7efa8d806ba19b58f29cb63c600d
Daniel Leaver <dleaverfr@gmail.com>
2021-12-07 18:22:01 +0100

    FizzBuzz

---
+++ src/main/java/com/algorithms/puzzles/FizzBuzz.java
@@ -0,0 +1,7 @@
+package com.algorithms.puzzles;
+
+public class FizzBuzz {
+    public static String classify(int n) {
+        return n % 2 == 0 ? "Fizz" : "Buzz";
+    }
+}
---
+++ src/test/java/com/algorithms/puzzles/FizzBuzzTest.java
@@ -0,0 +1,25 @@
+package com.algorithms.puzzles;
+
+import org.junit.Test;
+
+public class FizzBuzzTest {
+    @Test
+    public void isReturnsFizzOnEven() {
+        assert FizzBuzz.classify(0).equals("Fizz");
+        assert FizzBuzz.classify(2).equals("Fizz");
+        assert FizzBuzz.classify(-2).equals("Fizz");
+        assert FizzBuzz.classify(4).equals("Fizz");
+        assert FizzBuzz.classify(10).equals("Fizz");
+        assert FizzBuzz.classify(660).equals("Fizz");
+    }
+
+    @Test
+    public void isReturnsBuzzOnOdd() {
+        assert FizzBuzz.classify(1).equals("Buzz");
+        assert FizzBuzz.classify(-1).equals("Buzz");
+        assert FizzBuzz.classify(-5).equals("Buzz");
+        assert FizzBuzz.classify(9).equals("Buzz");
+        assert FizzBuzz.classify(11).equals("Buzz");
+        assert FizzBuzz.classify(661).equals("Buzz");
+    }
+}
