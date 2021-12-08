29df03da550eaa697e0e8a9e1182bd7a5b929709
Daniel Leaver <dleaverfr@gmail.com>
2021-12-08 14:51:15 +0100

    ThreeInOne

---
+++ src/main/java/com/algorithms/puzzles/ThreeInOne.java
@@ -0,0 +1,80 @@
+package com.algorithms.puzzles;
+
+import java.util.Arrays;
+import java.util.NoSuchElementException;
+
+public class ThreeInOne {
+    private int[] values;
+    private int[] starts;
+    private int[] lengths;
+
+    public ThreeInOne() {
+        values = new int[9];
+        starts = new int[] { 0, 3, 6 };
+        lengths = new int[] { 0, 0, 0 };
+    }
+
+    public void push(int stack, int value) {
+        if (isStackFull(stack)) growStacks();
+        values[starts[stack] + lengths[stack]++] = value;
+    }
+
+    public int peek(int stack) {
+        if (isStackEmpty(stack)) throw new NoSuchElementException("Stack is empty");
+        return values[starts[stack] + lengths[stack] - 1];
+    }
+
+    public int pop(int stack) {
+        if (isStackEmpty(stack)) throw new NoSuchElementException("Stack is empty");
+        return values[starts[stack] + lengths[stack]-- - 1];
+    }
+
+    public int size(int stack) {
+        return lengths[stack];
+    }
+
+    private boolean isStackFull(int stack) {
+        return isLastStack(stack)
+            ? starts[stack] + lengths[stack] == values.length
+            : starts[stack] + lengths[stack] == starts[stack + 1];
+    }
+
+    private boolean isStackEmpty(int stack) {
+        return lengths[stack] == 0;
+    }
+
+    private boolean isFirstStack(int stack) {
+        return stack == 0;
+    }
+
+    private boolean isLastStack(int stack) {
+        return stack == 2;
+    }
+
+    private void growStacks() {
+        var newLengths = new int[] { 0, 0 ,0 };
+        var newStarts = new int[] { 0, 0, 0 };
+        for (var i = 0; i <= 2; i++) {
+            newLengths[i] = shouldStackGrow(i) ? lengths[i] * 2 : lengths[i];
+            newStarts[i] = isFirstStack(i) ? 0 : newStarts[i - 1] + Math.max(newLengths[i - 1], 3);
+        }
+        var newValues = new int[Arrays.stream(newLengths).map(n -> Math.max(n, 3)).sum()];
+        for (var i = 0; i <= 2; i++) {
+            System.arraycopy(
+                    values,
+                    starts[i],
+                    newValues,
+                    newStarts[i],
+                    lengths[i]);
+        }
+        values = newValues;
+    }
+
+    private boolean shouldStackGrow(int stack) {
+        var remainingCapacity = isLastStack(stack)
+                ? values.length - (starts[stack] + lengths[stack])
+                : starts[stack + 1] - (starts[stack] + lengths[stack]);
+
+        return remainingCapacity < lengths[stack];
+    }
+}
---
+++ src/test/java/com/algorithms/puzzles/ThreeInOneTest.java
@@ -0,0 +1,136 @@
+package com.algorithms.puzzles;
+
+import org.junit.Test;
+
+import java.util.NoSuchElementException;
+
+public class ThreeInOneTest {
+
+    @Test
+    public void canAddToStack() {
+        var stack = new ThreeInOne();
+        assert stack.size(1) == 0;
+        stack.push(1, 10);
+        assert stack.size(1) == 1;
+    }
+
+    @Test
+    public void canAddToStackAfterGrowth() {
+        var stack = new ThreeInOne();
+        assert stack.size(1) == 0;
+        stack.push(1, 10);
+        stack.push(1, 10);
+        stack.push(1, 10);
+        stack.push(1, 10);
+        assert stack.size(1) == 4;
+    }
+
+    @Test
+    public void onlyOneStackGrowsIfOthersDoNotNeedTo() {
+        var stack = new ThreeInOne();
+        assert stack.size(0) == 0;
+        assert stack.size(1) == 0;
+        assert stack.size(2) == 0;
+        stack.push(1, 10);
+        stack.push(1, 10);
+        stack.push(1, 10);
+        stack.push(1, 10);
+        assert stack.size(0) == 0;
+        assert stack.size(1) == 4;
+        assert stack.size(2) == 0;
+    }
+
+    @Test
+    public void manyStacksGrowIfTheyNeedTo() {
+        var stack = new ThreeInOne();
+        assert stack.size(0) == 0;
+        assert stack.size(1) == 0;
+        assert stack.size(2) == 0;
+        stack.push(1, 10);
+        stack.push(1, 10);
+        stack.push(1, 10);
+        stack.push(1, 10);
+        stack.push(2, 10);
+        stack.push(2, 10);
+        stack.push(2, 10);
+        stack.push(2, 10);
+        assert stack.size(0) == 0;
+        assert stack.size(1) == 4;
+        assert stack.size(2) == 4;
+    }
+
+    @Test
+    public void peekWorks() {
+        var stack = new ThreeInOne();
+        stack.push(1, 10);
+        assert stack.peek(1) == 10;
+    }
+
+    @Test
+    public void peekWorksAfterGrowth() {
+        var stack = new ThreeInOne();
+        stack.push(1, 10);
+        stack.push(1, 9);
+        stack.push(1, 8);
+        stack.push(1, 7);
+        assert stack.peek(1) == 7;
+    }
+
+    @Test
+    public void peekCanBeChained() {
+        var stack = new ThreeInOne();
+        stack.push(1, 10);
+        assert stack.peek(1) == 10;
+        assert stack.peek(1) == 10;
+    }
+
+    @Test
+    public void cantPeekEmptyStack() {
+        var stack = new ThreeInOne();
+        try {
+            stack.peek(1);
+        } catch (NoSuchElementException e) {
+            assert e.getMessage().equals("Stack is empty");
+        }
+    }
+
+    @Test
+    public void popWorks() {
+        var stack = new ThreeInOne();
+        stack.push(1, 10);
+        assert stack.pop(1) == 10;
+    }
+
+    @Test
+    public void popWorksAfterGrowth() {
+        var stack = new ThreeInOne();
+        stack.push(1, 10);
+        stack.push(1, 9);
+        stack.push(1, 8);
+        stack.push(1, 7);
+        assert stack.pop(1) == 7;
+    }
+
+    @Test
+    public void popIsLIFO() {
+        var stack = new ThreeInOne();
+        stack.push(1, 10);
+        stack.push(1, 9);
+        stack.push(1, 8);
+        stack.push(1, 7);
+        assert stack.pop(1) == 7;
+        assert stack.pop(1) == 8;
+        assert stack.pop(1) == 9;
+        assert stack.pop(1) == 10;
+    }
+
+    @Test
+    public void cantPopEmptyStack() {
+        var stack = new ThreeInOne();
+        try {
+            stack.pop(1);
+        } catch (NoSuchElementException e) {
+            assert e.getMessage().equals("Stack is empty");
+        }
+    }
+}
