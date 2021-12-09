8eed1fd713a2b9d70c550278c85c936a89908222
Daniel Leaver <dleaverfr@gmail.com>
2021-12-09 12:49:07 +0100

    QueueViaStacks

---
+++ src/main/java/com/algorithms/puzzles/QueueViaStack.java
@@ -0,0 +1,50 @@
+package com.algorithms.puzzles;
+
+import java.util.Stack;
+
+public class QueueViaStack<T> extends Stack<T> {
+  private Stack<T> stack, kcast;
+
+  public QueueViaStack() {
+    stack = new Stack<>();
+    kcast = new Stack<>();
+  }
+
+  @Override
+  public T peek() {
+    shiftToKcast();
+    return kcast.peek();
+  }
+
+  @Override
+  public T pop() {
+    shiftToKcast();
+    return kcast.pop();
+  }
+
+  @Override
+  public T push(T element) {
+    shiftToStack();
+    stack.push(element);
+    return element;
+  }
+
+  @Override
+  public int size() {
+    return Math.max(stack.size(), kcast.size());
+  }
+
+  private void shiftToKcast() {
+    var times = stack.size();
+    for (var i = 0; i < times; i++) {
+      kcast.push(stack.pop());
+    }
+  }
+
+  private void shiftToStack() {
+    var times = kcast.size();
+    for (var i = 0; i < times; i++) {
+      stack.push(kcast.pop());
+    }
+  }
+}
---
+++ src/test/java/com/algorithms/puzzles/QueueViaStackTest.java
@@ -0,0 +1,49 @@
+package com.algorithms.puzzles;
+
+import org.junit.Test;
+
+public class QueueViaStackTest {
+
+  @Test
+  public void pushes() {
+    var stack = new QueueViaStack<Integer>();
+    stack.push(10);
+    assert stack.size() == 1;
+    stack.push(9);
+    stack.push(8);
+    assert stack.size() == 3;
+  }
+
+  @Test
+  public void peeks() {
+    var stack = new QueueViaStack<Integer>();
+    stack.push(10);
+    assert stack.peek() == 10;
+    stack.push(9);
+    stack.push(8);
+    assert stack.peek() == 10;
+  }
+
+  @Test
+  public void pops() {
+    var stack = new QueueViaStack<Integer>();
+    stack.push(10);
+    assert stack.pop() == 10;
+    stack.push(9);
+    stack.push(8);
+    assert stack.pop() == 9;
+    assert stack.pop() == 8;
+  }
+
+  @Test
+  public void pushesPeeksAndPops() {
+    var stack = new QueueViaStack<Integer>();
+    stack.push(10);
+    stack.push(9);
+    stack.push(8);
+    assert stack.pop() == 10;
+    assert stack.pop() == 9;
+    assert stack.peek() == 8;
+    assert stack.pop() == 8;
+  }
+}
