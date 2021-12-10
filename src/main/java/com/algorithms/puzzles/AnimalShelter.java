55c1515655665a299878204c039a7a5cf0f00c0f
Daniel Leaver <dleaverfr@gmail.com>
2021-12-10 10:30:48 +0100

    AnimalShelter

---
+++ src/main/java/com/algorithms/puzzles/AnimalShelter.java
@@ -0,0 +1,52 @@
+package com.algorithms.puzzles;
+
+import java.util.LinkedList;
+import java.util.NoSuchElementException;
+
+public class AnimalShelter {
+  private static class Animal {
+    private final long order;
+    private final String name;
+
+    public Animal(long order, String name) {
+      this.order = order;
+      this.name = name;
+    }
+  }
+
+  private long order;
+  private LinkedList<Animal> dogs;
+  private LinkedList<Animal> cats;
+
+  public AnimalShelter() {
+    this.order = 0;
+    this.cats = new LinkedList<>();
+    this.dogs = new LinkedList<>();
+  }
+
+  public String enqueueDog(String name) {
+    dogs.push(new Animal(order++, name));
+    return name;
+  }
+
+  public String enqueueCat(String name) {
+    cats.push(new Animal(order++, name));
+    return name;
+  }
+
+  public String dequeue() throws NoSuchElementException {
+    var cat = cats.peek();
+    var dog = dogs.peek();
+    if (cat == null) return dogs.pop().name;
+    if (dog == null) return cats.pop().name;
+    return cat.order > dog.order ? cats.pop().name : dogs.pop().name;
+  }
+
+  public String dequeueDog() throws NoSuchElementException {
+    return dogs.pop().name;
+  }
+
+  public String dequeueCat() throws NoSuchElementException {
+    return cats.pop().name;
+  }
+}
---
+++ src/test/java/com/algorithms/puzzles/AnimalShelterTest.java
@@ -0,0 +1,37 @@
+package com.algorithms.puzzles;
+
+import org.junit.Test;
+
+public class AnimalShelterTest {
+
+  @Test
+  public void enqueueDequeueCats() {
+    var shelter = new AnimalShelter();
+    shelter.enqueueCat("Lizzy");
+    shelter.enqueueCat("Raven");
+    assert shelter.dequeueCat().equals("Raven");
+    assert shelter.dequeueCat().equals("Lizzy");
+  }
+
+  @Test
+  public void enqueueDequeueDogs() {
+    var shelter = new AnimalShelter();
+    shelter.enqueueDog("Spot");
+    shelter.enqueueDog("Ralf");
+    assert shelter.dequeueDog().equals("Ralf");
+    assert shelter.dequeueDog().equals("Spot");
+  }
+
+  @Test
+  public void dequeues() {
+    var shelter = new AnimalShelter();
+    shelter.enqueueDog("Spot");
+    shelter.enqueueDog("Ralf");
+    shelter.enqueueCat("Lizzy");
+    shelter.enqueueCat("Raven");
+    assert shelter.dequeue().equals("Raven");
+    assert shelter.dequeue().equals("Lizzy");
+    assert shelter.dequeue().equals("Ralf");
+    assert shelter.dequeue().equals("Spot");
+  }
+}
