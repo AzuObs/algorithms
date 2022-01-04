9e43cf51aa2f4076b26859ce1c4e987ec5e40a50
Daniel Leaver <dleaverfr@gmail.com>
2022-01-04 10:40:56 +0100

    SmallestDifference

--- src/main/java/com/algorithms/puzzles/ctci/SmallestDifference.java
+++ src/main/java/com/algorithms/puzzles/ctci/SmallestDifference.java
@@ -1,2 +1,26 @@
-package com.algorithms.puzzles.ctci;public class SmallestDifference {
+package com.algorithms.puzzles.ctci;
+
+import java.util.Arrays;
+
+public class SmallestDifference {
+
+  public static int get(int[] as, int[] bs) {
+    if (as.length == 0 || bs.length == 0) throw new ArithmeticException("Can't find diff between empty arrays");
+    Arrays.sort(bs);
+    var difference = Integer.MAX_VALUE;
+    for (int a: as) {
+      var diff = findClosestB(a, bs, 0, bs.length - 1);
+      if (diff < difference) difference = diff;
+    }
+    return difference;
+  }
+
+  private static int findClosestB(int a, int[] sortedBs, int lb, int ub) {
+    if (lb == ub) return Math.abs(a - sortedBs[lb]);
+    var medianIndex = (ub + lb) / 2;
+    var median = sortedBs[medianIndex];
+    return a <= median
+        ? findClosestB(a, sortedBs, lb, medianIndex)
+        : findClosestB(a, sortedBs, medianIndex + 1, ub);
+  }
 }
--- src/test/java/com/algorithms/puzzles/ctci/SmallestDifferenceTest.java
+++ src/test/java/com/algorithms/puzzles/ctci/SmallestDifferenceTest.java
@@ -1,2 +1,13 @@
-package com.algorithms.puzzles.ctci;public class SmallestDifferenceTest {
+package com.algorithms.puzzles.ctci;
+
+import org.junit.Test;
+
+public class SmallestDifferenceTest {
+
+  @Test(expected = ArithmeticException.class) public void emptyAs() { SmallestDifference.get(new int[0], new int[] { 1 }); }
+  @Test(expected = ArithmeticException.class) public void emptyBs() { SmallestDifference.get(new int[] { 1 }, new int[0]); }
+  @Test public void smallAs() { assert SmallestDifference.get(new int[] { 2 }, new int[] { 0, 3, 4, 5 }) == 1;}
+  @Test public void smallBs() { assert SmallestDifference.get(new int[] { 0, 3, 4, 5 }, new int[] { 2 }) == 1;}
+  @Test public void regular() { assert SmallestDifference.get(new int[] { 2, 10, 20, 100 }, new int[] { 5, 30, 63, 102 }) == 2;}
+
 }
