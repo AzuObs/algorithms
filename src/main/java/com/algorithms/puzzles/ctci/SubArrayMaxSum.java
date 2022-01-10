a6d2cf0e2645d3accd360a059ddb99a71ac46077
Daniel Leaver <dleaverfr@gmail.com>
2022-01-10 11:14:43 +0100

    SubArrayMaxSum

---
+++ src/main/java/com/algorithms/puzzles/ctci/SubArrayMaxSum.java
@@ -0,0 +1,16 @@
+package com.algorithms.puzzles.ctci;
+
+public class SubArrayMaxSum {
+
+  public static int get(int[] arr) {
+    var maxSum = Integer.MIN_VALUE;
+    var runningSum = new int[arr.length];
+    for (var i = 0; i < arr.length; i++) {
+      if (i == 0) runningSum[0] = arr[0];
+      else runningSum[i] = runningSum[i - 1] > 0 ? runningSum[i - 1] + arr[i]  : arr[i]; }
+    for (var i = 0; i < arr.length; i++) {
+      maxSum = Math.max(runningSum[i], maxSum);
+    }
+    return maxSum;
+  }
+}
